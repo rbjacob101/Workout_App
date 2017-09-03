ef557946c97b727562bf578f8a2454ccb7d73203
rbjacob101 <rbjacob101@gmail.com>
2017-09-02 22:22:42 -0600

    last commit fail logo.png; activity_main.xml; MainActivity.java added splash and then home page

--- app/src/main/java/jsrb/com/workoutapp/MainActivity.java
+++ app/src/main/java/jsrb/com/workoutapp/MainActivity.java
@@ -1,7 +1,12 @@
 package jsrb.com.workoutapp;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //remove actionBar
+        getSupportActionBar().hide();
+
+        //wait a few seconds and then hide logo image and restore actionBar
+        new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick (long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                findViewById(R.id.imageView).setVisibility(View.GONE);
+                getSupportActionBar().show();
+            }
+
+        }.start();
+
+
+
+
     }
 }
+
