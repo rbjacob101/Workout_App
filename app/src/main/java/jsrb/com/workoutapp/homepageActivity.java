5bc5eeac395bb31dc5c6fffe174020793859126f
rbjacob101 <rbjacob101@gmail.com>
2017-09-10 15:30:17 -0600

    changed gradient colors again; added basic dynamic gradient set switcher on homepageActivity.java

--- app/src/main/java/jsrb/com/workoutapp/homepageActivity.java
+++ app/src/main/java/jsrb/com/workoutapp/homepageActivity.java
@@ -1,6 +1,8 @@
 package jsrb.com.workoutapp;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -30,6 +32,17 @@ public class homepageActivity extends AppCompatActivity {
         gradientBackgroundPainter.stop();
     }
 
+    /*transitionTest gradient from dynamic gradient 1 to dynamic gradient 2
+    must be integrated into sliding between fragments and changing gradient theme
+     */
+
+    public void transitionGradient (View view) {
+        gradientBackgroundPainter.stop();
+        View layout = getWindow().getDecorView();
+        layout.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.transitionTest));
+        TransitionDrawable transition = (TransitionDrawable) layout.getBackground();
+        transition.startTransition(100);
+    }
 
     //do nothing if the user presses android back button
     @Override public void onBackPressed() {
